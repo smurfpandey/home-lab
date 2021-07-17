f9528d440847b3f2672ae693c121bcdfe5f6b065
Neeraj Verma <github@smurfpandey.me>
2021-07-17 13:12:41 +0530

    try terraform for homelab

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.2"
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Smurfville"
+
+    workspaces {
+      name = "home-lab"
+    }
+  }
+}
+
+provider "cloudflare" { }
