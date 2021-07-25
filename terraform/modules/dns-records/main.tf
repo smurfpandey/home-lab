1d047521e87f7a48f2a980c488d843d50a1a4e20
Neeraj Verma <github@smurfpandey.me>
2021-07-25 21:44:34 +0530

    create domain for sync calendars app

--- terraform/modules/dns-records/main.tf
+++ terraform/modules/dns-records/main.tf
@@ -21,3 +21,13 @@ resource "cloudflare_record" "playday" {
   ttl     = 1
   proxied = true
 }
+
+resource "cloudflare_record" "sync_cal" {
+  zone_id = data.cloudflare_zones.smurfville_app.zones[0].id
+  name    = "sync-cal"
+  value   = var.do1_cname
+  type    = "CNAME"
+
+  ttl     = 1
+  proxied = true
+}
